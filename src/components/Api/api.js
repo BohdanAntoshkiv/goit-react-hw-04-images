359fbaffc2ee5d9717f5ec004567ea1332dddac2
Bohdan <Antoshkivbl@gmail.com>
2022-10-21 22:15:58 +0300

    bug modal

--- src/components/Api/api.js
+++ src/components/Api/api.js
@@ -2,20 +2,21 @@
 
 import axios from 'axios';
 
+const API_KEY = '29801102-c735e5df67f9ec5a047fa49a5';
 
 export const getImages = async (query, page) => {
     const response = await axios.get(
         `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-        );
-        if (response.status !== 200) {
-            throw new Error(`Bad response, ${response.status}`);
-        }
-        return response.data.hits.map(formatImage);
-    };
+    );
+    if (response.status !== 200) {
+        throw new Error(`Bad response, ${response.status}`);
+    }
+    return response.data.hits.map(formatImage);
+};
 
-    const formatImage = image => ({
-        tags: image.tags,
-        webformatURL: image.webformatURL,
-        largeImageURL: image.largeImageURL,
-        id: image.id,
-    });
+const formatImage = image => ({
+    tags: image.tags,
+    webformatURL: image.webformatURL,
+    largeImageURL: image.largeImageURL,
+    id: image.id,
+});
