78e17a60e16c0b36eb6b4d428751945d2c9c0f8a
Bohdan <Antoshkivbl@gmail.com>
2022-11-03 00:16:50 +0200

    finish hw-5

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,136 +1,7 @@
-import { useState, useEffect, useRef } from 'react';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { getImages } from 'components/Api/api';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Button } from 'components/Button/Button';
-import { Loader } from 'components/Loader/Loader';
+import PropTypes from 'prop-types';
 
-export default function ImageGallery({ queryName }) {
-    // const [queryName, setQueryName] = useState('');
-    const [images, setImages] = useState([]);
-    const [isLoading, setLoading] = useState(false);
-    const [page, setPage] = useState(1);
-
-    const isFirstRender = useRef(true);
-
-    useEffect(() => {
-        if (isFirstRender.current) {
-            isFirstRender.current = false;
-            return;
-        }
-
-        if (queryName === '') {
-            return;
-        }
-
-        async function foo() {
-            try {
-                setLoading(true);
-                const response = await getImages(queryName, page);
-                if (response.length === 0) {
-                    Notify.failure('Nothing founded');
-                    setLoading(false);
-                    return;
-                }
-                setImages(prev => [...prev, ...response]);
-                setLoading(false);
-            } catch (error) {
-                Notify.failure(error.message);
-            }
-        }
-
-        foo();
-    }, [queryName, page]);
-
-    const onLoadMore = () => {
-        setPage(page => page + 1);
-    };
-
-    // setQueryName(() => {
-    //     if (queryName.trim() === '') {
-    //         Notify.failure('Nothing to search');
-    //         return;
-    //     }
-    //     setQueryName(queryName);
-    //     setImages([]);
-    //     setPage(1);
-    // });
-
-    //   const filterChanging = (newQuery) => {
-    //     if (newQuery === queryName || newQuery.trim() === '') {
-    //       Notify.failure('Nothing to search');
-    //       return;
-    //     }
-    //     setQueryName(newQuery);
-    //     setImages([]);
-    //     setPage(1);
-    //   }
-
-    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // state = {
-    //     images: [],
-    //     isLoading: false,
-    //     page: 1,
-    //     lastPage: null,
-    // };
-
-    // componentDidUpdate = async (prevProps, prevState) => {
-    //     const { queryName } = this.props;
-    //     const { page } = this.state;
-    //     const pageInFetch = prevProps.queryName !== queryName ? 1 : page;
-    //     if (prevProps.queryName !== queryName || prevState.page !== page) {
-    //         if (queryName === '') {
-    //             Notify.failure('Nothing to search');
-    //             return;
-    //         }
-
-    //         try {
-    //             this.setState({ isLoading: true });
-    //             const response = await getImages(queryName, pageInFetch);
-
-    //             if (response.length !== 0) {
-    //                 const maxPage =
-    //                     response.length < 12 ? this.state.page : null;
-    //                 this.setState({ lastPage: maxPage });
-    //             }
-    //             if (response.length === 0) {
-    //                 Notify.failure('Nothing founded');
-    //                 this.setState({ isLoading: false });
-    //                 return;
-    //             }
-    //             if (prevProps.queryName !== queryName) {
-    //                 this.setState({ images: response, page: 1 });
-    //             }
-    //             if (prevState.page !== page && page !== 1) {
-    //                 this.setState(state => ({
-    //                     images: [...state.images, ...response],
-    //                 }));
-    //             }
-    //             this.setState({ isLoading: false });
-    //         } catch (error) {
-    //             Notify.failure(error.message);
-    //         }
-    //     }
-    // };
-
-    // onLoadMore = () => {
-    //     this.setState(state => ({ page: state.page + 1 }));
-    // };
-
-    const showButton = () => {
-        // const { images, isLoading, lastPage } = this.state;
-        if (images.length === 0) {
-            return;
-        }
-        if (isLoading === true) {
-            return;
-        }
-        // if (lastPage) {
-        //     return;
-        // }
-        return <Button onLoad={onLoadMore}>LOAD MORE...</Button>;
-    };
-
+export default function ImageGallery({ images }) {
     return (
         <>
             <ul className="ImageGallery">
@@ -138,8 +9,10 @@ export default function ImageGallery({ queryName }) {
                     <ImageGalleryItem key={image.id} image={image} />
                 ))}
             </ul>
-            {showButton()}
-            {isLoading && <Loader />}
         </>
     );
 }
+
+ImageGallery.propTypes = {
+    images: PropTypes.array.isRequired,
+};
