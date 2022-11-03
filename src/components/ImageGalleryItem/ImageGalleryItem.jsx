8e9c17742880279cffdbb43b1303dd34df00d738
Bohdan <Antoshkivbl@gmail.com>
2022-11-03 19:35:28 +0200

    bug ImageItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -4,7 +4,7 @@ import { Modal } from 'components/Modal/Modal';
 
 export function ImageGalleryItem({ image }) {
     const { webformatURL, largeImageURL, tags } = image;
-    const [isModalOpened, setModalOpened] = useState();
+    const [isModalOpened, setModalOpened] = useState(false);
 
     const toggleModal = () => {
         setModalOpened(!isModalOpened);
